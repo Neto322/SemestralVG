269abc3476053aabd580e09a308ed1995ad8465d
Neto322 <manuel123317@hotmail.com>
2021-05-03 01:54:02 -0500

    Online con Photon
    a

--- Assets/ConnectionManager.cs
+++ Assets/ConnectionManager.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
 using MLAPI.Spawning;
-using System;
+
 
 public class ConnectionManager : MonoBehaviour
-{
+{   
+    Vector3 Position;
 
     //Pasa del lado del server.
     public void Host()
     {
         NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
-        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.StartHost(RandomPosition(),Quaternion.identity);
     }
 
     private void ApprovalCheck(byte[] connectionData, ulong clientID, NetworkManager.ConnectionApprovedDelegate callback)
     {
         bool approve = System.Text.Encoding.ASCII.GetString(connectionData) == "Password1234";
-        callback(true,null,approve,Vector3.zero,Quaternion.identity);
+        callback(true,null,approve,  RandomPosition() ,Quaternion.identity);
     }
 
     public void Join()
@@ -33,5 +34,15 @@ public class ConnectionManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    Vector3 RandomPosition()
+    {
+
+        float X = Random.Range(-5.0f, 5.0f);
+        float Y = Random.Range(-5.0f, 5.0f);
+
+        return new Vector3(X,Y,0);
+
     }
 }
